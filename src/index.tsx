cfd2fc8daa7d2be5b40b3645e8b60737ce84bb46
Yasu1025 <sillyjp25@gmail.com>
2022-01-15 16:27:43 +0900

    esbuild setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as esbuild from 'esbuild-wasm'
+import React, { useEffect, useState, useRef } from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-  return <h1>aaa</h1>
+const App = () => {
+  const ref = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const onClick = () => {
+    if (!ref.current) return
+
+    // TODO
+    console.log(ref.current)
+  }
+
+  return (
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)} />
+      <div>
+        <button type='submit' onClick={onClick}>
+          Submit
+        </button>
+        <pre>{code}</pre>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
