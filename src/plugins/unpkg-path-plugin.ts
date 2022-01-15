37cae0503d249a24b74705f71c3d98695b58e8cb
Yasu1025 <sillyjp25@gmail.com>
2022-01-15 17:54:02 +0900

    Esmodule plugin setup

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,31 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as esbuild from 'esbuild-wasm'
+
+export const unpkgPathPlugin = () => ({
+  name: 'unpkg-path-plugin',
+  setup(build: esbuild.PluginBuild) {
+    build.onResolve({ filter: /.*/ }, async (args: any) => {
+      console.log('onResole', args)
+      return { path: args.path, namespace: 'a' }
+    })
+
+    // TODO: Dynamic fetching and loading NPM modules
+    build.onLoad({ filter: /.*/ }, async (args: any) => {
+      console.log('onLoad', args)
+
+      if (args.path === 'index.js') {
+        return {
+          loader: 'jsx',
+          contents: `
+              import message from './message';
+              console.log(message);
+            `,
+        }
+      }
+      return {
+        loader: 'jsx',
+        contents: 'export default "hi there!"',
+      }
+    })
+  },
+})
